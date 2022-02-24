ea028f3da80551968439ded9443e58b7b8bc24e1
Chris Rishel <rishel@gmail.com>
2022-02-23 18:48:59 -0800

    Throws UserNotLoggedIn when private practice group article accessed and no user is logged in

---
+++ src/Hook/BeforeInitialize.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PracticeGroups\Hook;
+
+use MediaWiki;
+use OutputPage;
+use PracticeGroups\PracticeGroups;
+use Title;
+use User;
+use UserNotLoggedIn;
+use WebRequest;
+
+class BeforeInitialize {
+    public static function callback( Title &$title, $unused, OutputPage $output, User $user, WebRequest $request, MediaWiki $mediaWiki ) {
+        $title = $output->getTitle();
+        $user = $output->getUser();
+
+        if( PracticeGroups::isTitlePracticeGroupArticle( $title ) &&
+            !PracticeGroups::userCanReadPracticeGroupTitle( $title ) &&
+            !$user->isLoggedIn() ) {
+            throw new UserNotLoggedIn();
+        }
+    }
+}
